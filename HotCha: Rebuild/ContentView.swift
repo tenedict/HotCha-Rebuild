e2789f94db1dacf0588326fa7253edbc9d5e9e34
tenedict <tenedict@gmail.com>
2025-01-31 19:02:26 +0900

    [Feat]프로젝트 생성

---
+++ HotCha: Rebuild/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotCha: Rebuild
+//
+//  Created by 문재윤 on 1/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotCha: Rebuild/HotCha__RebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotCha__RebuildApp.swift
+//  HotCha: Rebuild
+//
+//  Created by 문재윤 on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct HotCha__RebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
