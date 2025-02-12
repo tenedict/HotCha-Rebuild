1213b772171a73974d9039e78d4072c2ce4ea5be
Ho Mun <mhhh2082@gmail.com>
2025-02-13 01:47:20 +0900

    feat: UIFont 추가

---
+++ HotCha/HotCha/Resources/UIFont.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont.swift
+//  HotCha
+//
+//  Created by 문호 on 2/13/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let heading1: Font = .custom("Pretendard-Bold", size: 24)
+    static let heading2: Font = .custom("Pretendard-Medium", size: 24)
+    static let heading3: Font = .custom("Pretendard-SemiBold", size: 20)
+    
+    static let sub_heading1: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let sub_heading2: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let sub_heading3: Font = .custom("Pretendard-Medium", size: 16)
+    
+    static let body1: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let body2: Font = .custom("Pretendard-Medium", size: 14)
+    
+    static let caption: Font = .custom("Pretendard-Medium", size: 12)
+}
